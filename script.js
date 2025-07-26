9cd54e39c6c756c4a1f11bb2fbcb4fdb4022ecba
venkata surendra reddy Allampati <surendraprince483@gmail.com>
2025-07-26 18:37:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Typing Effect
+const typingText = document.querySelector(".typing-text");
+const textArray = [
+  "Cyber Security Enthusiast",
+  "AI & Python Developer",
+  "Data Science Learner",
+  "Full-Stack Web Developer"
+];
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    typingText.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", type);
